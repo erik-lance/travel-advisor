4fd50702ed71035f2968ebb2da9ecd2be51f6d21
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2022-01-16 21:32:18 +0800

    Added Starting Questions.

--- main.pl
+++ main.pl
@@ -4,3 +4,25 @@
  * Mangoba, Michael
  * Tiongquico, Erik
 */
+% input(Question) :-
+
+input(Question) :-
+
+write('Trip Advisor Agent (TAA) Israel').
+write('Would you like to travel to Israel?'),
+read(answer), nl,
+    ((answer == yes; answer  == y)
+    -> 
+
+    assert(yes(Question));
+    assert(no(Question), fail).
+
+    )
+
+:- dynamic yes/1,no/1.
+
+verify(Order) :-
+    (yes(Order)) :-
+        -> true;
+    (no(Order)) :-
+        -> fail;
